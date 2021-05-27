a0ef616b02336cd30420b0bbc4f4fb2b50caa744
flaviomoreni <fmoreni@gmail.com>
2021-05-27 20:02:14 -0300

    classe de debug e ordenação lazy

---
+++ src/OrdemCrescenteLazy.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class OrdemCrescenteLazy {
+
+	public static void main(String[] args) {
+
+		int valores[] = { 18, 37, 61, 5, 33, 100, 34, 1, 76, 66, 44, 12, 80, 11, 31, 72, 16, 3, 5, 94, 49, 7, 75, 32,
+				79 };
+		
+		Arrays.sort(valores);
+		
+		System.out.println(Arrays.toString(valores));
+		
+		
+	}
+
+}
---
+++ src/TesteDebug.java
@@ -0,0 +1,38 @@
+
+public class TesteDebug {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Print 1");
+
+		System.out.println("Print 2");
+
+		System.out.println("Print 3");
+
+		System.out.println("Print 4");
+
+		boolean professor2 = true;
+		boolean professor1 = false;
+		boolean professor3 = true;
+		boolean professor4 = false;
+		boolean professor5 = true;
+		boolean professor6 = false;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		if ( professor1 == true && professor2 == true) {
+			System.out.println("Print 5");
+		}
+	
+	}
+
+}
